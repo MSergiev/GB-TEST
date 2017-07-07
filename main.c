2669b7128f5fdca9ce384fab3809bf33e525412a
Miroslav Sergiev <sergievmiroslav@gmail.com>
2017-07-07 21:10:32 +0300

    Added text field

--- main.c
+++ main.c
@@ -2,7 +2,10 @@
 #include <gb/gb.h>
 #include <gb/drawing.h>
 
+#define V_OFF 25
+
 UINT8 keyState, oldState;
+UINT8 lineCounter;
 
 void setButtonColor();
 void setPressedColor();
@@ -16,15 +19,26 @@ void drawLEFT();
 void drawRIGHT();
 void drawSELECT();
 void drawSTART();
+void printKey();
 void eventHandler();
 
 int main() {
+	lineCounter = 0;
 	drawGameBoy();
+	line(50,0,50,GRAPHICS_HEIGHT);
 	while(1){
 		eventHandler();
 		if(keyState!=oldState){
+			printKey();
+			//line(50,0,50,GRAPHICS_HEIGHT);
+			//drawGameBoy();
 			drawButtons();
 			oldState = keyState;
+			if(lineCounter>=36){
+				color(WHITE,WHITE,SOLID);
+				box(0,0,49,GRAPHICS_HEIGHT,1);
+			   	lineCounter = 0;
+			}
 		}
 		keyState = 0;
 	}
@@ -34,17 +48,17 @@ int main() {
 void drawGameBoy(){
 	//Draw body
 	color(DKGREY, BLACK, SOLID); 
-	box(45,15,115,130,1);
+	box(45+V_OFF,15,115+V_OFF,130,1);
 	color(DKGREY, BLACK, SOLID); 
-	box(45,15,115,130,1);
+	box(45+V_OFF,15,115+V_OFF,130,1);
 	//Draw screen frame
 	color(BLACK, DKGREY, SOLID);
-	box(50,25,110,65,1);
+	box(50+V_OFF,25,110+V_OFF,65,1);
 	//Draw LED
-	circle(55,40,1,1);
+	circle(55+V_OFF,40,1,1);
 	//Draw screen
 	color(BLACK, LTGREY, SOLID);
-	box(60,28,100,62,1);
+	box(60+V_OFF,28,100+V_OFF,62,1);
 	//Draw buttons
 	drawButtons();
 }
@@ -60,77 +74,69 @@ void setPressedColor(){
 void drawA(){
 	if(keyState&128) setPressedColor();	
 	else setButtonColor();
-	circle(105,93,5,1);
+	circle(105+V_OFF,93,5,1);
 	setPressedColor();
-	circle(105,93,5,0);
+	circle(105+V_OFF,93,5,0);
 }
 
 void drawB(){
-	if(keyState&64) setPressedColor();	
+	if(keyState&64) setPressedColor();
 	else setButtonColor();	
-	circle(92,97,5,1);
+	circle(92+V_OFF,97,5,1);
 	setPressedColor();
-	circle(92,97,5,0);
+	circle(92+V_OFF,97,5,0);
 }
 
 void drawUP(){
 	if(keyState&32) setPressedColor();	
 	else setButtonColor();	
-	circle(58,88,3,1);
+	circle(58+V_OFF,88,3,1);
 	setPressedColor();
-	line(55,85,61,85);
-	line(55,85,55,91);
-	line(61,85,61,91);
+	line(55+V_OFF,85,61+V_OFF,85);
+	line(55+V_OFF,85,55+V_OFF,91);
+	line(61+V_OFF,85,61+V_OFF,91);
 }
 
 void drawDOWN(){
 	if(keyState&16) setPressedColor();	
 	else setButtonColor();	
-	circle(58,98,3,1);
+	circle(58+V_OFF,98,3,1);
 	setPressedColor();
-	line(55,101,61,101);
-	line(55,95,55,101);
-	line(61,95,61,101);
+	line(55+V_OFF,101,61+V_OFF,101);
+	line(55+V_OFF,95,55+V_OFF,101);
+	line(61+V_OFF,95,61+V_OFF,101);
 }
 
 void drawLEFT(){
 	if(keyState&8) setPressedColor();	
 	else setButtonColor();	
-	circle(53,93,3,1);
+	circle(53+V_OFF,93,3,1);
 	setPressedColor();
-	line(50,90,55,90);
-	line(50,90,50,96);
-	line(50,96,55,96);
+	line(50+V_OFF,90,55+V_OFF,90);
+	line(50+V_OFF,90,50+V_OFF,96);
+	line(50+V_OFF,96,55+V_OFF,96);
 }
 
 void drawRIGHT(){
 	if(keyState&4) setPressedColor();	
 	else setButtonColor();	
-	circle(63,93,3,1);
+	circle(63+V_OFF,93,3,1);
 	setPressedColor();
-	line(61,90,66,90);
-	line(66,90,66,96);
-	line(61,96,66,96);
+	line(61+V_OFF,90,66+V_OFF,90);
+	line(66+V_OFF,90,66+V_OFF,96);
+	line(61+V_OFF,96,66+V_OFF,96);
 }
 
 void drawSTART(){
 	if(keyState&2) setPressedColor();
 	else setButtonColor();
-	circle(80,116,1,1);
-	circle(82,116,1,1);
-	circle(84,116,1,1);
-	setPressedColor();
-	box(79,115,85,117,0);
+	box(79+V_OFF,115,85+V_OFF,117,1);
 }
 
 void drawSELECT(){
 	if(keyState&1) setPressedColor();
 	else setButtonColor();
-	circle(71,116,1,1);
-	circle(73,116,1,1);
-	circle(75,116,1,1);
-	setPressedColor();
-	box(70,115,76,117,0);
+	box(70+V_OFF,115,76+V_OFF,117,1);
 }
 
 void drawButtons(){
@@ -142,6 +148,20 @@ void drawButtons(){
 	drawRIGHT();
 	drawSTART();
 	drawSELECT();
+}
+
+void printKey(){
+	color(BLACK,WHITE,SOLID);
+	gotogxy(0, lineCounter/2);
+	if(keyState&1) gprintf("SELECT");
+	else if(keyState&2) gprintf("START ");
+	else if(keyState&4) gprintf("RIGHT ");
+	else if(keyState&8) gprintf("LEFT  ");
+	else if(keyState&16) gprintf("DOWN  ");
+	else if(keyState&32) gprintf("UP    ");
+	else if(keyState&64) gprintf("B     ");
+	else if(keyState&128) gprintf("A     ");
+	lineCounter++;
 }
 
 void eventHandler(){
